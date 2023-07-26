5acf97c31a531c2bb49d309215bfdb35a7b0af74
时间淡忘一切 <7990497@qq.com>
2023-07-26 11:36:26 +0800

    refactor: APT自动编译重构，只要鼠标移开编辑器就会触发自动编译，文件时候正确由用户自行确认

--- src/main/java/club/bigtian/mf/plugin/core/MybatisFlexDocumentChangeHandler.java
+++ src/main/java/club/bigtian/mf/plugin/core/MybatisFlexDocumentChangeHandler.java
@@ -5,7 +5,6 @@ import club.bigtian.mf.plugin.core.util.PsiJavaFileUtil;
 import club.bigtian.mf.plugin.core.util.VirtualFileUtils;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.compiler.CompilerManager;
@@ -13,51 +12,96 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.event.DocumentEvent;
-import com.intellij.openapi.editor.event.DocumentListener;
-import com.intellij.openapi.editor.event.EditorFactoryEvent;
-import com.intellij.openapi.editor.event.EditorFactoryListener;
+import com.intellij.openapi.editor.event.*;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.psi.KtFile;
 
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 
 /**
  * @author bigtian
  */
-public class MybatisFlexDocumentChangeHandler implements DocumentListener, EditorFactoryListener, Disposable {
+public class MybatisFlexDocumentChangeHandler implements DocumentListener, EditorFactoryListener {
     private static final Logger LOG = Logger.getInstance(MybatisFlexDocumentChangeHandler.class);
-    private final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newScheduledThreadPool(1);
-    private ScheduledFuture<?> scheduledFuture;
-
+    private static final Key<Boolean> CHANGE = Key.create("change");
+    private static final Key<Boolean> LISTENER = Key.create("listener");
 
     public MybatisFlexDocumentChangeHandler() {
         super();
         // 所有的文档监听
-        EditorFactory.getInstance().getEventMulticaster().addDocumentListener(this, this);
+        EditorFactory.getInstance().getEventMulticaster().addDocumentListener(this);
+        Document document;
+        for (Editor editor : EditorFactory.getInstance().getAllEditors()) {
+            document = editor.getDocument();
+            document.putUserData(LISTENER, true);
+            editor.addEditorMouseListener(new EditorMouseListener() {
+                @Override
+                public void mouseExited(@NotNull EditorMouseEvent event) {
+                    executeCompile(editor);
+                }
+            });
+        }
     }
 
+    @Override
+    public void editorReleased(@NotNull EditorFactoryEvent event) {
+        Editor editor = event.getEditor();
+
+        Document document = editor.getDocument();
+        if (Boolean.TRUE.equals(document.getUserData(LISTENER))) {
+            document.putUserData(LISTENER, false);
+            document.removeDocumentListener(this);
+        }
+    }
 
     @Override
     public void editorCreated(@NotNull EditorFactoryEvent event) {
         EditorFactoryListener.super.editorCreated(event);
         Editor editor = event.getEditor();
+        editor.addEditorMouseListener(new EditorMouseListener() {
+            @Override
+            public void mouseExited(@NotNull EditorMouseEvent event) {
+                executeCompile(editor);
+            }
+        });
         Document document = editor.getDocument();
-        document.addDocumentListener(this);
+        if (Boolean.TRUE.equals(document.getUserData(LISTENER))) {
+            document.putUserData(LISTENER, true);
+            document.addDocumentListener(this);
+        }
     }
+
+    private void executeCompile(Editor editor) {
+        Document document = editor.getDocument();
+        boolean flag = checkFile(editor);
+        Project project = editor.getProject();
+        VirtualFile currentFile = VirtualFileUtils.getVirtualFile(document);
+        if (ObjectUtil.isNull(currentFile)) {
+            return;
+        }
+        PsiFile psiFile = VirtualFileUtils.getPsiFile(project, currentFile);
+        if (flag || ObjectUtil.isNull(currentFile) || !(psiFile instanceof PsiJavaFile) && !(psiFile instanceof KtFile)
+                || !ObjectUtil.defaultIfNull(document.getUserData(CHANGE), false)) {
+            return;
+        }
+        document.putUserData(CHANGE, false);
+        // 执行任务的逻辑
+        Application application = ApplicationManager.getApplication();
+        application.invokeLater(() -> {
+            compile(editor);
+        });
+    }
+
 
     /**
      * 校验文件是否导入了Table注解
@@ -73,7 +117,6 @@ public class MybatisFlexDocumentChangeHandler implements DocumentListener, Edito
         }
         PsiManager psiManager = PsiManager.getInstance(Objects.requireNonNull(editor.getProject()));
         PsiFile psiFile = psiManager.findFile(currentFile);
-
         // 支持java和kotlin
         if (!(psiFile instanceof PsiJavaFile) && !(psiFile instanceof KtFile)) {
             return false;
@@ -91,137 +134,20 @@ public class MybatisFlexDocumentChangeHandler implements DocumentListener, Edito
     }
 
     @Override
-    public void editorReleased(@NotNull EditorFactoryEvent event) {
-        EditorFactoryListener.super.editorReleased(event);
-        Editor editor = event.getEditor();
-        Document document = editor.getDocument();
-        document.removeDocumentListener(this);
-    }
-
-    @Override
     public void documentChanged(@NotNull DocumentEvent event) {
+        Document document = event.getDocument();
         CharSequence newFragment = event.getNewFragment();
         if ((StrUtil.isBlank(newFragment) && StrUtil.isBlank(event.getOldFragment()))) {
             return;
         }
-
-
-        Document document = event.getDocument();
-        EditorFactory.getInstance().editors(document).findAny().ifPresent(editor -> {
-            boolean flag = checkFile(editor);
-            if (flag) {
-                return;
-            }
-            Project project = editor.getProject();
-            VirtualFile currentFile = VirtualFileUtils.getVirtualFile(editor.getDocument());
-            if (ObjectUtil.isNull(currentFile)) {
-                return;
-            }
-            PsiFile psiFile = VirtualFileUtils.getPsiFile(project, currentFile);
-            if (!(psiFile instanceof PsiJavaFile) && !(psiFile instanceof KtFile)) {
-                return;
-            }
-            PsiClassOwner psiJavaFile = (PsiClassOwner) psiFile;
-            Runnable task = () -> {
-                // 执行任务的逻辑
-                Application application = ApplicationManager.getApplication();
-                application.invokeLater(() -> {
-                    PsiErrorElement errorElement = PsiTreeUtil.findChildOfType(psiJavaFile, PsiErrorElement.class);
-                    if (ObjectUtil.isNull(errorElement) && !isPsiErrorElement(psiJavaFile)) {
-                        compile(event);
-                    }
-                });
-            };
-            if (ObjectUtil.isNotNull(scheduledFuture)) {
-                scheduledFuture.cancel(true);
-            }
-            // 延迟触发任务
-            scheduledFuture = EXECUTOR_SERVICE.schedule(task, 800, TimeUnit.MILLISECONDS);
-        });
+        document.putUserData(CHANGE, true);
     }
 
-    private void compile(@NotNull DocumentEvent event) {
-        EditorFactory.getInstance().editors(event.getDocument()).findFirst().ifPresent(editor -> {
-            Project project = editor.getProject();
-            CompilerManager compilerManager = CompilerManager.getInstance(project);
-            VirtualFile currentFile = VirtualFileUtils.getVirtualFile(editor.getDocument());
-            LOG.warn("编译文件: " + currentFile.getName());
-            compilerManager.compile(new VirtualFile[]{currentFile}, null);
-            // TODO Kotlin的编译暂时不支持
-        });
-    }
-
-
-    /**
-     * 规避复制方法或者字段的时候也触发编译
-     *
-     * @param psiJavaFile
-     * @return
-     */
-    private boolean isPsiErrorElement(PsiClassOwner psiJavaFile) {
-        if (psiJavaFile instanceof KtFile) {
-            return false;
-        }
-        HashSet<String> elementSet = new HashSet<>();
-        PsiClass[] classes = psiJavaFile.getClasses();
-
-        for (PsiClass psiClass : classes) {
-            HashSet<Object> fieldSet = new HashSet<>();
-            for (PsiField field : psiClass.getFields()) {
-                if (fieldSet.contains(field.getName())) {
-                    return true;
-                }
-                fieldSet.add(field.getName());
-            }
-            PsiAnnotation annotation = psiClass.getAnnotation("lombok.Data");
-            for (PsiMethod psiMethod : psiClass.getMethods()) {
-                // 解决用户cv方法，导致编译错误
-                String text = psiMethod.getText();
-                for (PsiAnnotation psiMethodAnnotation : psiMethod.getAnnotations()) {
-                    text = text.replace(psiMethodAnnotation.getText(), "").trim();
-                }
-                PsiCodeBlock body = psiMethod.getBody();
-                assert body != null;
-                text = text.replace(body.getText(), "").trim();
-                if (elementSet.contains(text)) {
-                    return true;
-                } else {
-                    elementSet.add(text);
-                }
-                //TODO 注解必填项不填，导致编译错误
-                //get/set方法
-                if (ObjectUtil.isNull(annotation) && (psiMethod.getName().startsWith("get") || psiMethod.getName().startsWith("set"))) {
-                    // 解决用户修改字段后，get/set没有及时更新，导致编译报错
-                    if (psiMethod.getName().startsWith("get")) {
-                        String bodyText = body.getText();
-                        String aReturn = StrUtil.subBetween(bodyText, "return ", ";").trim();
-                        if (!fieldSet.contains(aReturn)) {
-                            return true;
-                        }
-                    } else if (psiMethod.getName().startsWith("set")) {
-                        String bodyText = body.getText();
-                        String aReturn = StrUtil.subBetween(bodyText, "this.", "=").trim();
-                        if (!fieldSet.contains(aReturn)) {
-                            return true;
-                        }
-                    }
-                }
-
-
-            }
-
-
-        }
-        return false;
-    }
-
-
-    @Override
-    public void dispose() {
-        if (ObjectUtil.isNotNull(scheduledFuture)) {
-            scheduledFuture.cancel(true);
-        }
-        EXECUTOR_SERVICE.shutdown();
-        Disposer.dispose(this);
+    private void compile(@NotNull Editor editor) {
+        Project project = editor.getProject();
+        CompilerManager compilerManager = CompilerManager.getInstance(project);
+        VirtualFile currentFile = VirtualFileUtils.getVirtualFile(editor.getDocument());
+        LOG.warn("编译文件: " + currentFile.getName());
+        compilerManager.compile(new VirtualFile[]{currentFile}, null);
     }
 }
