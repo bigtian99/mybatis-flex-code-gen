60a6f29c25a14b4574fcca6323db66d499e737c8
时间淡忘一切 <7990497@qq.com>
2023-07-31 15:12:38 +0800

    fix: 修复Velocity实例化出错

--- src/main/java/club/bigtian/mf/plugin/core/RenderMybatisFlexTemplate.java
+++ src/main/java/club/bigtian/mf/plugin/core/RenderMybatisFlexTemplate.java
@@ -18,8 +18,6 @@ import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.log.NullLogChute;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.StringWriter;
@@ -40,11 +38,12 @@ public class RenderMybatisFlexTemplate {
 
         VelocityEngine velocityEngine = new VelocityEngine();
         //修复因velocity.log拒绝访问，导致Velocity初始化失败
-        try {
-            velocityEngine.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM, new NullLogChute());
-        } catch (Exception e) {
-            LOG.error("Velocity初始化失败；如果不影响生成，请忽略");
-        }
+//        高版本已经把这个方法废弃了，所以这里注释掉；优先支持高版本
+//        try {
+//            velocityEngine.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM, new NullLogChute());
+//        } catch (Exception e) {
+//            LOG.error("Velocity初始化失败；如果不影响生成，请忽略");
+//        }
         VelocityContext context = new VelocityContext();
         HashMap<PsiDirectory, List<PsiElement>> templateMap = new HashMap<>();
         Map<String, String> templates = new ConcurrentHashMap<>(config.getTemplates());
